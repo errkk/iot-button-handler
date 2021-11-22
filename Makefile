4cff5626b02e86185661e0569078d246386b8532
Eric George <errkk@hey.com>
2021-11-22 18:33:19 +0000

    turn lights off

---
+++ Makefile
@@ -0,0 +1,28 @@
+.PHONY: venv_create run watch install test test_watch cli
+
+BIN=venv/bin/
+
+include .env
+export
+
+venv_create:
+	virtualenv -p python3 venv
+
+run:
+	$(BIN)python handler.py
+
+watch:
+	nodemon -e py --exec "$(BIN)python handler.py"
+
+install:
+	cat requirements.txt requirements.dev.txt | $(BIN)pip install -r /dev/stdin
+
+test:
+	$(BIN)pytest --cov=handler
+
+test_watch:
+	rm -rf src/.pytest_cache &&\
+	rm -rf src/.tmontmp &&\
+	rm -f src/.testmondata &&\
+	$(BIN)pytest-watch -- --testmon -vv --disable-pytest-warnings -rP
+
---
+++ handler.py
@@ -0,0 +1,36 @@
+from os import environ, path
+from requests import Session, Response
+
+access_token = environ.get('access_token')
+HUE_APPLICATION_KEY = environ.get('hue_application_key')
+
+BASE_URL = "https://api.meethue.com/route/clip/v2"
+
+DESK = "abb4dce9-a33a-4a7f-bf69-a491f8d61c22"
+BOWL = "15d8cf20-62db-471e-bbe4-d6835f80cfe4"
+LAMP = "f6f1012b-04b1-4970-9d67-81e60ca7cd92"
+
+hue_client = Session()
+hue_client.headers.update({
+    'Authorization': f"Bearer {access_token}",
+    'hue-application-key': HUE_APPLICATION_KEY,
+    'Allow': "*"
+})
+
+def url(pathname: str) -> str:
+    return path.join(BASE_URL, pathname)
+
+def on(light_id: str, on: bool) -> Response:
+    payload = {"on": {"on": on}}
+    return hue_client.put(url(f"resource/light/{light_id}"), json=payload)
+
+
+def all_off():
+    for light_id in [DESK, BOWL, LAMP]:
+        on(light_id, False)
+
+def main():
+    all_off()
+
+if __name__ == "__main__":
+    main()
